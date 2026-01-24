a4f66bef5a5ae4f338f8e136f8cf542654379d29
Henry Johnson <27johnhen@isd748.org>
2026-01-24 17:46:39 -0600

    create intake open loop

--- src/main/java/frc/robot/commands/IntakeCommands/IntakeOpenLoop.java
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeOpenLoop.java
@@ -1,5 +1,38 @@
 package frc.robot.commands.IntakeCommands;
 
-public class IntakeOpenLoop {
-    
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.subsystems.Intake;
+
+public class IntakeOpenLoop extends Command {
+  private final Intake m_Intake;
+  private final DoubleSupplier m_SpeedSupplier;
+
+  public IntakeOpenLoop(Intake intake, DoubleSupplier speedSupplier) {
+    m_Intake = intake;
+    m_SpeedSupplier = speedSupplier;
+    addRequirements(intake);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    double speed = MathUtil.applyDeadband(m_SpeedSupplier.getAsDouble(), ControllerConstants.DEADBAND);
+    m_Intake.setSpeed(speed);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_Intake.stopIntakeMotor();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
