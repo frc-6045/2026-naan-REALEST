cef28b0b611211394e19000178f3e9b8d38119dc
Aaron Huisinga <aaron@northbuilt.com>
2026-02-07 08:52:16 -0600

    Run feeder AND spindexer when running FeedToShooter command

--- src/main/java/frc/robot/commands/ShootFeedCommands/FeedToShooter.java
+++ src/main/java/frc/robot/commands/ShootFeedCommands/FeedToShooter.java
@@ -3,6 +3,7 @@ package frc.robot.commands.ShootFeedCommands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Spindexer;
 import frc.robot.Constants.MotorConstants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class FeedToShooter extends Command {
   private final Feeder m_feeder;
+  private final Spindexer m_spindexer;
   private final Shooter m_shooter;
   private final double m_targetRPM;
   private final boolean m_checkSpeed;
@@ -20,25 +22,28 @@ public class FeedToShooter extends Command {
   /**
    * Creates a new FeedToShooter command with shooter speed checking enabled.
    * @param feeder The feeder subsystem
+   * @param spindexer The spindexer subsystem
    * @param shooter The shooter subsystem (to check speed)
    */
-  public FeedToShooter(Feeder feeder, Shooter shooter) {
-    this(feeder, shooter, MotorConstants.kShooterTargetRPM, true);
+  public FeedToShooter(Feeder feeder, Spindexer spindexer, Shooter shooter) {
+    this(feeder, spindexer, shooter, MotorConstants.kShooterTargetRPM, true);
   }
 
   /**
    * Creates a new FeedToShooter command.
    * @param feeder The feeder subsystem
+   * @param spindexer The spindexer subsystem
    * @param shooter The shooter subsystem
    * @param targetRPM The expected shooter speed
    * @param checkSpeed Whether to verify shooter is at speed before feeding
    */
-  public FeedToShooter(Feeder feeder, Shooter shooter, double targetRPM, boolean checkSpeed) {
+  public FeedToShooter(Feeder feeder, Spindexer spindexer, Shooter shooter, double targetRPM, boolean checkSpeed) {
     m_feeder = feeder;
+    m_spindexer = spindexer;
     m_shooter = shooter;
     m_targetRPM = targetRPM;
     m_checkSpeed = checkSpeed;
-    addRequirements(feeder);
+    addRequirements(feeder, spindexer);
   }
 
   @Override
@@ -52,11 +57,13 @@ public class FeedToShooter extends Command {
     // Only feed if shooter is at target speed (or if check is disabled)
     if (!m_checkSpeed || m_shooter.isAtTargetSpeed(m_targetRPM)) {
       m_feeder.setSpeed(MotorConstants.kFeederShootSpeed);
+      m_spindexer.setSpeed(MotorConstants.kSpindexerIndexSpeed);
       SmartDashboard.putBoolean("Feeding", true);
       SmartDashboard.putBoolean("Shooter Ready", true);
     } else {
       // Shooter not ready yet, wait
       m_feeder.stopFeederMotor();
+      m_spindexer.stopSpindexerMotor();
       SmartDashboard.putBoolean("Feeding", false);
       SmartDashboard.putBoolean("Shooter Ready", false);
     }
@@ -64,8 +71,9 @@ public class FeedToShooter extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    // Stop the feeder when button is released
+    // Stop the feeder and spindexer when button is released
     m_feeder.stopFeederMotor();
+    m_spindexer.stopSpindexerMotor();
     SmartDashboard.putBoolean("Feeding", false);
   }
 
