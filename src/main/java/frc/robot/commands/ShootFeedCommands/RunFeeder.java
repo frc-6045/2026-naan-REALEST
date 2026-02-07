229f1813914cc319ad91e59fdb0f190c0a2a15fc
Seeinq <ayusfun1@gmail.com>
2026-02-07 15:53:33 -0600

    I don't think claude's cahnges will actually make difference

--- src/main/java/frc/robot/commands/ShootFeedCommands/RunFeeder.java
+++ src/main/java/frc/robot/commands/ShootFeedCommands/RunFeeder.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.ShootFeedCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.Directions;
 import frc.robot.Constants.MotorConstants;
 import frc.robot.subsystems.shooterSystem.Feeder;
@@ -40,6 +39,11 @@ public class RunFeeder extends Command {
         //     // If not running, start the roller at intake speed
         //     m_IntakeSubsystem.setSpeed(MotorConstants.kIntakeRollerSpeed);
         // }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
     }
 
     @Override
