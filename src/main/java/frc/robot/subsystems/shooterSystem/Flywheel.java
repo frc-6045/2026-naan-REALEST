c80147b224a3edafe79fd6316d8d872ba688d3b5
allison zhang <ayusfun1@gmail.com>
2026-03-03 21:53:36 -0600

    Revert "Revert uvw adjustments"
    This reverts commit e55589176cf3467b53e5af227cd18933f6db2ff3.

--- src/main/java/frc/robot/subsystems/shooterSystem/Flywheel.java
+++ src/main/java/frc/robot/subsystems/shooterSystem/Flywheel.java
@@ -82,9 +82,9 @@ public class Flywheel extends SubsystemBase {
         .d(MotorConstants.kShooterD)
         .velocityFF(MotorConstants.kShooterFF)
         .iZone(MotorConstants.kShooterIZone);
-    // m_config.encoder
-    //     .uvwAverageDepth(2)
-    //     .uvwMeasurementPeriod(10);
+    m_config.encoder
+        .uvwAverageDepth(2)
+        .uvwMeasurementPeriod(10);
   }
 
   public void setSpeed(double speed) {
