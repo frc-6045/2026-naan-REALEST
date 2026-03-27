b1237dc52207782c92f7992998694e87ab28bd02
allison zhang <ayusfun1@gmail.com>
2026-03-26 21:10:14 -0500

    Henry fix autos

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -174,17 +174,7 @@ public class Autos {
     m_autoChooser.setDefaultOption("None", null);
 
     // Add autos to chooser
-    m_autoChooser.addOption("half auto", AutoBuilder.buildAuto("quarterautonovisionAllison"));
-    m_autoChooser.addOption("full auto", AutoBuilder.buildAuto("moreautonovisionAllison"));
-    m_autoChooser.addOption("3/4 auto", AutoBuilder.buildAuto("34autonovisionAllison"));
-    m_autoChooser.addOption("outpost auto", AutoBuilder.buildAuto("outpost auto"));
-    m_autoChooser.addOption("half auto swoop", AutoBuilder.buildAuto("quarterautonovisionwithbigswoop"));
-    m_autoChooser.addOption("full auto swoop", AutoBuilder.buildAuto("moreautonovisionwithbigswoop"));
-    m_autoChooser.addOption("3/4 auto swoop", AutoBuilder.buildAuto("34autonovisionwithbigswoop"));
-    m_autoChooser.addOption("test intake pivot", AutoBuilder.buildAuto("teststow"));
     m_autoChooser.addOption("45 degree auto", AutoBuilder.buildAuto("45 degree start"));
-    
-    m_autoChooser.addOption("test", AutoBuilder.buildAuto("New Auto"));
 
     SmartDashboard.putData("Auto Chooser", m_autoChooser);
   }
