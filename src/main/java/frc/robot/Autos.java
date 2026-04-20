5536f2877703e0ab8501bc9e25773a7c6bfd9fca
Henry Johnson <27johnhen@isd748.org>
2026-04-20 16:32:18 -0500

    Update Autos.java

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -178,19 +178,20 @@ public class Autos {
     m_autoChooser = new SendableChooser<>();
     m_autoChooser.setDefaultOption("None", null);
 
-    // Add autos to chooser (right side)
-    m_autoChooser.addOption("RIGHT mid-length", AutoBuilder.buildAuto("-new45"));
-    m_autoChooser.addOption("RIGHT long", AutoBuilder.buildAuto("LONG of -new45"));
-    m_autoChooser.addOption("RIGHT short for with 4607", AutoBuilder.buildAuto("SHORT of -new45"));
-    m_autoChooser.addOption("RIGHT small mid-length", AutoBuilder.buildAuto("small -new45 right"));
-    m_autoChooser.addOption("RIGHT small short", AutoBuilder.buildAuto("Copy short small -new45 right"));
-
-    // Add autos to chooser (left side - mirrored)
-    m_autoChooser.addOption("LEFT mid-length", AutoBuilder.buildAuto("LEFT -new45"));
-    m_autoChooser.addOption("LEFT long", AutoBuilder.buildAuto("LEFT LONG of -new45"));
-    m_autoChooser.addOption("LEFT short for with 4607", AutoBuilder.buildAuto("LEFT SHORT of -new45"));
-    m_autoChooser.addOption("LEFT small mid-length", AutoBuilder.buildAuto("small -new45 left"));
-    m_autoChooser.addOption("LEFT small short", AutoBuilder.buildAuto("Copy short small -new45 left"));
+    
+    m_autoChooser.addOption("RIGHT mid-length", AutoBuilder.buildAuto("RIGHT mid-length"));
+    m_autoChooser.addOption("RIGHT short", AutoBuilder.buildAuto("RIGHT short"));
+    m_autoChooser.addOption("RIGHT long", AutoBuilder.buildAuto("RIGHT long"));
+    m_autoChooser.addOption("RIGHT small midlength", AutoBuilder.buildAuto("RIGHT small midlength"));
+    m_autoChooser.addOption("RIGHT small short", AutoBuilder.buildAuto("RIGHT small short"));
+    m_autoChooser.addOption("RIGHT double bump", AutoBuilder.buildAuto("RIGHT double bump"));
+    m_autoChooser.addOption("LEFT mid-length", AutoBuilder.buildAuto("LEFT mid-length"));
+    m_autoChooser.addOption("LEFT short", AutoBuilder.buildAuto("LEFT short"));
+    m_autoChooser.addOption("LEFT long", AutoBuilder.buildAuto("LEFT long"));
+    m_autoChooser.addOption("LEFT small midlength", AutoBuilder.buildAuto("LEFT small midlength"));
+    m_autoChooser.addOption("LEFT small short", AutoBuilder.buildAuto("LEFT small short"));
+    m_autoChooser.addOption("LEFT double bump", AutoBuilder.buildAuto("LEFT double bump"));
+    m_autoChooser.addOption("CENTER depot", AutoBuilder.buildAuto("CENTER depot"));
 
     SmartDashboard.putData("Auto Chooser", m_autoChooser);
   }
